d3f6d1fc3641b5dbc4dc810df4962244f6bffc6e
Web-Coder2023 <denis.tihonkih.frontend.dumix@yandex.ru>
2024-08-25 11:38:53 +0600

    ...

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import VideoPlayer from "./components/VideoPlayer.jsx";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <VideoPlayer />
+    </div>
+  );
+};
+
+export default App;
